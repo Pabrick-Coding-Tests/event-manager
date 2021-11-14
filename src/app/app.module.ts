69d71e5ec24a09f0d6f2dba7115b6871980f5d7d
Pabrick <pabrick.bastaky@gmail.com>
2021-11-14 14:57:03 +0000

    Add initial components

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventListComponent } from './pages/event-list/event-list.component';
+import { EventFormComponent } from './pages/event-form/event-form.component';
+import { CardEventComponent } from './components/card-event/card-event.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventListComponent,
+    EventFormComponent,
+    CardEventComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/card-event/card-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-event',
+  templateUrl: './card-event.component.html',
+  styleUrls: ['./card-event.component.scss']
+})
+export class CardEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
