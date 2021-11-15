30364fe647fc97274a5cb87c34fa9bac6dc76c52
Pabrick <pabrick.bastaky@gmail.com>
2021-11-15 19:27:35 +0000

    Add List page

--- src/app/pages/event-list/event-list.component.spec.ts
+++ src/app/pages/event-list/event-list.component.spec.ts
@@ -1,13 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { EventListComponent } from "./event-list.component";
 
-import { EventListComponent } from './event-list.component';
-
-describe('EventListComponent', () => {
+describe("EventListComponent", () => {
   let component: EventListComponent;
   let fixture: ComponentFixture<EventListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ EventListComponent ]
     })
     .compileComponents();
@@ -19,7 +20,12 @@ describe('EventListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should render title", () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector("p")?.textContent).toContain("event-list works!");
   });
 });
--- src/app/pages/event-list/event-list.component.ts
+++ src/app/pages/event-list/event-list.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { map, Observable } from "rxjs";
+import { EventStorageService } from "../../services/storage/event-storage.service";
+import { EventItem } from "../../models/event-item.interface";
 
 @Component({
-  selector: 'app-event-list',
-  templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.scss']
+  selector: "event-list",
+  templateUrl: "./event-list.component.html",
+  styleUrls: ["./event-list.component.scss"]
 })
 export class EventListComponent implements OnInit {
 
-  constructor() { }
+  public eventIdSelected$: Observable<string | null>;
+  public upcomingEventList$: Observable<EventItem[] | null>;
+  public pastEventList$: Observable<EventItem[] | null>;
 
-  ngOnInit(): void {
+  constructor(
+    public readonly route: ActivatedRoute,
+    public readonly router: Router,
+    private readonly _eventStorageSrv: EventStorageService
+  ) {
+    this.eventIdSelected$ = this.route.queryParams.pipe(
+      map((params: Params) => params["id"])
+    );
+    this.upcomingEventList$ = this._eventStorageSrv.getEventList$().pipe(
+      map((eventList: EventItem[]) => {
+        return eventList; // Add filter logic here
+      })
+    );
+    this.pastEventList$ = this._eventStorageSrv.getEventList$().pipe(
+      map((eventList: EventItem[]) => {
+        return eventList; // Add filter logic here
+      })
+    )
+  }
+
+  ngOnInit(): void {}
+
+  public createNewEvent(): void {
+    this.router.navigate(["./form"]);
+  }
+
+  public onUpdateEvent(event: EventItem): void {
+    console.log("onUpdateEvent", event);
+    this.router.navigate(["./form"], { queryParams: { "id": event.id }});
+  }
+
+  public onDeleteEvent(event: EventItem): void {
+    console.log("onDeleteEvent", event);
   }
 
 }
