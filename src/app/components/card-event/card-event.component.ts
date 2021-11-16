703cdda869bd06a6878c9866154fd7a24da2c8eb
Pabrick <pabrick.bastaky@gmail.com>
2021-11-16 20:24:18 +0000

    Add styles to card comp

--- src/app/components/card-event/card-event.component.ts
+++ src/app/components/card-event/card-event.component.ts
@@ -7,15 +7,20 @@ import { EventItem } from "./../../models/event-item.interface";
   styleUrls: ["./card-event.component.scss"]
 })
 export class CardEventComponent implements OnInit {
-
-  @Input() item?: EventItem;
-  @Input() opened?: boolean;
+  @Input() item!: EventItem;
+  @Input() opened!: boolean;
   @Output() updateEmt: EventEmitter<boolean> = new EventEmitter();
   @Output() deleteEmt: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  public day?: string;
+  public month?: string;
 
   ngOnInit(): void {
+    const splittedDate = this.item.date.split("/");
+    const monthNames = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+    const monthNumber = parseInt(splittedDate[1], 10);
+    this.day = splittedDate[0];
+    this.month = monthNames[monthNumber];
   }
 
   public updateItem(): void {
